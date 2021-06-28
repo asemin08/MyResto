4daa0e7cea8bcbef46f2999552ef1d9e7f5d7fd8
flav1-ann <Moitoi28>
2021-06-28 15:07:32 +0200

    Product DTO and Test createProductService

--- Services/src/main/java/IProductService.java
+++ Services/src/main/java/IProductService.java
@@ -5,8 +5,16 @@ import java.util.Set;
 
 public interface IProductService {
     int createProduct(Product product) throws ServiceException, exceptions.ServiceException;
-    Set<Product> getAllProducts() throws ServiceException, exceptions.ServiceException;
+
+    Set<ProductDto> getAllProducts() throws ServiceException, exceptions.ServiceException;
+
     int updateProduct(Product product) throws ServiceException, exceptions.ServiceException;
+
     int deleteProduct(int idProduct) throws ServiceException, exceptions.ServiceException;
-    Product getOneProduct(int idProduct) throws ServiceException, exceptions.ServiceException;
+
+    ProductDto getOneProduct(int idProduct) throws ServiceException, exceptions.ServiceException;
+
+    Product convertProductDtoToProduct(ProductDto ProductDto);
+
+    ProductDto convertProductToProductDto(Product Product);
 }
--- Services/src/test/java/ProductServiceTest.java
+++ Services/src/test/java/ProductServiceTest.java
@@ -1,13 +1,20 @@
 import eu.ensup.myresto.domaine.Product;
+import exceptions.DaoException;
+import exceptions.ServiceException;
 import org.hamcrest.MatcherAssert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.HashSet;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ProductServiceTest {
 
     @Mock
@@ -17,15 +24,68 @@ public class ProductServiceTest {
     ProductService productService;
 
     @Test
-    public void CreateProductTest(){
+    public void ProductCreateTest(){
         try{
             Product product = new Product("CocaService", 3.5f, "", "C'est du coca");
-            var res = productService.createProduct(new Product("CocaService", 3.5f, "", "C'est du coca"));
-            when(res).thenReturn(1);
-            MatcherAssert.assertThat(res, equalTo(1));
-            verify(productDao).createProduct(new Product("CocaService", 3.5f, "", "C'est du coca"));
-        }catch (Exception e ){
-
+            when(productDao.createProduct(product)).thenReturn(1);
+            MatcherAssert.assertThat(productService.createProduct(product), equalTo(1));
+            verify(productDao).createProduct(product);
+        } catch (DaoException | ServiceException e) {
+            //TODO
         }
     }
+//    @Test
+//    public void ProductgetAllTest(){
+//        try{
+//            var res = productService.getAllProducts();
+//            when(res).thenReturn(new HashSet<>(){
+//                new Product("CocaService", 3.5f, "", "C'est du coca"),
+//            });
+//            MatcherAssert.assertThat(res, equalTo(1));
+//            verify(productDao).createProduct(new Product("CocaService", 3.5f, "", "C'est du coca"));
+//
+//        } catch (ServiceException | DaoException e) {
+//            //TODO
+//        }
+//    }
+//    @Test
+//    public void ProductDeleteTest(){
+//
+//        try{
+//            Product product = new Product("CocaService", 3.5f, "", "C'est du coca");
+//            var res = productService.createProduct(new Product("CocaService", 3.5f, "", "C'est du coca"));
+//            when(res).thenReturn(1);
+//            MatcherAssert.assertThat(res, equalTo(1));
+//            verify(productDao).createProduct(new Product("CocaService", 3.5f, "", "C'est du coca"));
+//        } catch (ServiceException | DaoException e) {
+//            //TODO
+//        }
+//    }
+//    @Test
+//    public void ProductGetOneTest(){
+//
+//        try{
+//            Product product = new Product("CocaService", 3.5f, "", "C'est du coca");
+//            var res = productService.createProduct(new Product("CocaService", 3.5f, "", "C'est du coca"));
+//            when(res).thenReturn(1);
+//            MatcherAssert.assertThat(res, equalTo(1));
+//            verify(productDao).createProduct(new Product("CocaService", 3.5f, "", "C'est du coca"));
+//        } catch (ServiceException | DaoException e) {
+//            //TODO
+//        }
+//    }
+//    @Test
+//    public void ProductUpdateTest(){
+//
+//        try{
+//            Product product = new Product("CocaService", 3.5f, "", "C'est du coca");
+//            var res = productService.createProduct(new Product("CocaService", 3.5f, "", "C'est du coca"));
+//            when(res).thenReturn(1);
+//            MatcherAssert.assertThat(res, equalTo(1));
+//            verify(productDao).createProduct(new Product("CocaService", 3.5f, "", "C'est du coca"));
+//        } catch (ServiceException | DaoException e) {
+//            //TODO
+//        }
+//    }
+
 }
