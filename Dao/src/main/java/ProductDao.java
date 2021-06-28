1d3c1156686f5b3fcfc5a546bbe9526bea030f4a
maximeDzN <maxime.dazin@gmail.com>
2021-06-28 13:47:59 +0200

    user dao exception & mockito add

--- Dao/src/main/java/ProductDao.java
+++ Dao/src/main/java/ProductDao.java
@@ -1,3 +1,4 @@
+import eu.ensup.myresto.domaine.Product;
 import exceptions.DaoException;
 
 import java.sql.SQLException;
@@ -12,13 +13,13 @@ public class ProductDao extends BaseDao implements IProductDao {
             connexion();
             String sql = "INSERT INTO `product`(name,price, description,image) VALUES (?,?,?,?)";
             BaseDao.setPs(BaseDao.getCn().prepareStatement(sql));
-            BaseDao.getPs().setString(1,product.getName());
-            BaseDao.getPs().setFloat(2,product.getPrice());
-            BaseDao.getPs().setString(3,product.getDescription() );
+            BaseDao.getPs().setString(1, product.getName());
+            BaseDao.getPs().setFloat(2, product.getPrice());
+            BaseDao.getPs().setString(3, product.getDescription());
             BaseDao.getPs().setString(4, product.getPicture());
             BaseDao.setResult(BaseDao.getPs().executeUpdate());
-        }catch (SQLException e) {
-            throw new DaoException(ProductDao.class.getName(),"createProduct",e.getMessage(),"Une erreur s'est produite lors de la récupération du produit");
+        } catch (SQLException e) {
+            throw new DaoException(ProductDao.class.getName(), "createProduct", e.getMessage(), "Une erreur s'est produite lors de la récupération du produit");
         }
         return BaseDao.getResult();
     }
@@ -32,10 +33,10 @@ public class ProductDao extends BaseDao implements IProductDao {
             BaseDao.setPs(BaseDao.getCn().prepareStatement(sql));
             BaseDao.setRs(BaseDao.getPs().executeQuery());
             while (BaseDao.getRs().next()) {
-                products.add(new Product(BaseDao.getRs().getInt("id"),BaseDao.getRs().getString("name"),BaseDao.getRs().getFloat("price"),BaseDao.getRs().getString("description"),BaseDao.getRs().getString("image")));
+                products.add(new Product(BaseDao.getRs().getInt("id"), BaseDao.getRs().getString("name"), BaseDao.getRs().getFloat("price"), BaseDao.getRs().getString("description"), BaseDao.getRs().getString("image")));
             }
         } catch (SQLException e) {
-            throw new DaoException(ProductDao.class.getName(),"getAllProducts",e.getMessage(),"Une erreur s'est produite lors de la récupération de tout les produits");
+            throw new DaoException(ProductDao.class.getName(), "getAllProducts", e.getMessage(), "Une erreur s'est produite lors de la récupération de tout les produits");
         }
         return products;
     }
@@ -55,7 +56,7 @@ public class ProductDao extends BaseDao implements IProductDao {
             disconnect();
 
         } catch (SQLException e) {
-            throw new DaoException(ProductDao.class.getName(),"updateProduct",e.getMessage(),"Une erreur s'est produite lors de la mise à jour du produit");
+            throw new DaoException(ProductDao.class.getName(), "updateProduct", e.getMessage(), "Une erreur s'est produite lors de la mise à jour du produit");
         }
         return BaseDao.getResult();
     }
@@ -66,28 +67,29 @@ public class ProductDao extends BaseDao implements IProductDao {
             connexion();
             String sql = "DELETE FROM `product` WHERE id=?";
             BaseDao.setPs(BaseDao.getCn().prepareStatement(sql));
-            BaseDao.getPs().setInt(1,idProduct);
+            BaseDao.getPs().setInt(1, idProduct);
             BaseDao.setResult(BaseDao.getPs().executeUpdate());
         } catch (SQLException | DaoException e) {
-            throw new DaoException(ProductDao.class.getName(),"deleteProduct",e.getMessage(),"Une erreur s'est produite lors de la suppression du produit");
+            throw new DaoException(ProductDao.class.getName(), "deleteProduct", e.getMessage(), "Une erreur s'est produite lors de la suppression du produit");
         }
         return BaseDao.getResult();
     }
 
     @Override
-    public Product getOneProduct(int idProduct) throws DaoException{
+    public Product getOneProduct(int idProduct) throws DaoException {
         try {
             connexion();
             String sql = "SELECT * FROM `product` WHERE id=?";
             BaseDao.setPs(BaseDao.getCn().prepareStatement(sql));
-            BaseDao.getPs().setInt(1,idProduct);
+            BaseDao.getPs().setInt(1, idProduct);
             BaseDao.setRs(BaseDao.getPs().executeQuery());
             while (getRs().next()) {
-                return new Product(BaseDao.getRs().getInt("id"),BaseDao.getRs().getString("name"),BaseDao.getRs().getFloat("price"),BaseDao.getRs().getString("description"),BaseDao.getRs().getString("image"));
+                return new Product(BaseDao.getRs().getInt("id"), BaseDao.getRs().getString("name"), BaseDao.getRs().getFloat("price"), BaseDao.getRs().getString("description"), BaseDao.getRs().getString("image"));
             }
         } catch (SQLException | DaoException e) {
-            throw new DaoException(ProductDao.class.getName(),"getOneProduct",e.getMessage(),"Une erreur s'est produite lors de la récuperation du produit");
+            throw new DaoException(ProductDao.class.getName(), "getOneProduct", e.getMessage(), "Une erreur s'est produite lors de la récuperation du produit");
         }
         return null;
 
+    }
 }
--- Dao/src/main/java/UserDao.java
+++ Dao/src/main/java/UserDao.java
@@ -1,11 +1,12 @@
 import eu.ensup.myresto.domaine.User;
+import exceptions.DaoException;
 
 import java.sql.SQLException;
 
 public class UserDao extends BaseDao implements IUserDao {
 
     @Override
-    public int create(User user) {
+    public int create(User user) throws DaoException {
         connexion();
         try {
             setPs(getCn().prepareStatement("INSERT INTO user(firstname,lastname,address,role,password,salt,image) VALUES(?,?,?,?,?,?,?) "));
@@ -19,14 +20,14 @@ public class UserDao extends BaseDao implements IUserDao {
             setResult(getPs().executeUpdate());
             disconnect();
             return getResult();
-        } catch (SQLException throwables) {
-            //TODO NOT IMPLEMENTED YET
-            return -1;
+        } catch (SQLException e) {
+            throw new DaoException(UserDao.class.getName(),"create",e.getMessage(),"Une erreur s'est produite lors de la création de l'utilisateur");
+
         }
     }
 
     @Override
-    public int update(User user) {
+    public int update(User user) throws DaoException  {
         connexion();
         try {
             setPs(getCn().prepareStatement("UPDATE user SET firstname = ?, lastname = ?, address = ?, role = ?, password = ?, salt = ?, image = ? where id = ?"));
@@ -41,14 +42,13 @@ public class UserDao extends BaseDao implements IUserDao {
             setResult(getPs().executeUpdate());
             disconnect();
             return getResult();
-        } catch (SQLException throwables) {
-            //TODO NOT IMPLEMENTED YET
-            return -1;
+        } catch (SQLException e) {
+            throw new DaoException(UserDao.class.getName(),"create",e.getMessage(),"Une erreur s'est produite lors de la mise à jour d'utilisateur");
         }
     }
 
     @Override
-    public int delete(int userId) {
+    public int delete(int userId) throws DaoException  {
         connexion();
         try {
             setPs(getCn().prepareStatement("delete from user where id = ?"));
@@ -56,15 +56,15 @@ public class UserDao extends BaseDao implements IUserDao {
             setResult(getPs().executeUpdate());
             disconnect();
             return getResult();
-        } catch (SQLException throwables) {
-            //TODO NOT IMPLEMENTED YET
-            return -1;
+        } catch (SQLException e) {
+            throw new DaoException(UserDao.class.getName(),"create",e.getMessage(),"Une erreur s'est produite lors de la suppression de l'utilisateur");
+
         }
     }
 
 
     @Override
-    public User getById(int userId) {
+    public User getById(int userId) throws DaoException {
         connexion();
         User getUser = null;
         try {
@@ -76,9 +76,9 @@ public class UserDao extends BaseDao implements IUserDao {
             }
             disconnect();
             return getUser;
-        } catch (SQLException throwables) {
-            //TODO NOT IMPLEMENTED YET
-            return null;
+        } catch (SQLException e) {
+            throw new DaoException(UserDao.class.getName(),"create",e.getMessage(),"Une erreur s'est produite lors de la récupération de l'utilisateur");
+
         }
     }
 
