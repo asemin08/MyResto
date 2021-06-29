3cec096071f02f8eaf20e488fad958ef56c362be
flav1-ann <Moitoi28>
2021-06-29 20:53:29 +0200

    Orders connection back front

--- Presentation/src/main/java/servelet/ServletOrders.java
+++ Presentation/src/main/java/servelet/ServletOrders.java
@@ -1,19 +1,42 @@
 package servelet;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import eu.ensup.myresto.OrderProductService;
+import eu.ensup.myresto.User;
+import eu.ensup.myresto.UserDto;
+import eu.ensup.myresto.exceptions.ServiceException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ServletOrder", value = "/orders")
 public class ServletOrders extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        this.getServletContext().getRequestDispatcher("/account/orders.jsp").forward(request,response);
+        operations(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        this.getServletContext().getRequestDispatcher("/account/orders.jsp").forward(request,response);
+        operations(request, response);
+    }
+
+    protected void operations(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        OrderProductService orderProductService = new OrderProductService();
+        HttpSession userSession = request.getSession();
+
+        try {
+            var orders = orderProductService.getAllOrderProductsForOneUser(((UserDto)request.getSession().getAttribute("user")).getId());
+            userSession.setAttribute("listOrders", orders);
+
+        } catch (ServiceException e) {
+            userSession.setAttribute("error", e.getMessageViewForUser());
+
+        }
+        this.getServletContext().getRequestDispatcher("/orders.jsp").forward(request, response);
     }
 }
