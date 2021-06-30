0a4e1eef2a15297a06d05339b678f410b2def791
flav1-ann <Moitoi28>
2021-06-30 10:50:12 +0200

    add robu for servlet

--- Presentation/src/main/java/servelet/ServletAddToOrder.java
+++ Presentation/src/main/java/servelet/ServletAddToOrder.java
@@ -15,17 +15,25 @@ public class ServletAddToOrder extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        operations(request, response);
+        HttpSession userSession = request.getSession();
+        try {
+            operations(request, response,userSession);
+        } catch (ServletException | IOException e) {
+            userSession.setAttribute("error", e.getMessage());
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        operations(request, response);
+        HttpSession userSession = request.getSession();
+        try {
+            operations(request, response,userSession);
+        } catch (ServletException | IOException e) {
+            userSession.setAttribute("error", e.getMessage());
+        }
     }
 
-    protected void operations(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        HttpSession userSession = request.getSession();
+    protected void operations(HttpServletRequest request, HttpServletResponse response,HttpSession userSession) throws ServletException, IOException {
         List<Integer> productList = new ArrayList<>();
         if (userSession.getAttribute("order") == null) {
             productList.add(Integer.parseInt(request.getParameter("productId")));
@@ -36,7 +44,6 @@ public class ServletAddToOrder extends HttpServlet {
         userSession.setAttribute("order", productList);
         System.out.println(userSession.getAttribute("order").toString());
         response.sendRedirect(request.getContextPath()+"/menu");
-        //this.getServletContext().getRequestDispatcher("/menu.jsp").forward(request, response);
 
     }
 
